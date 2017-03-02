2d912d69c6265ec8f1a2c96c308d76731bd2bda0
blacop <81643216@qq.com>
2017-03-02 12:08:57 +0800

    TEST

--- DSCLS/List/单链表/单链表 一次封装 整理 管炳耀.c
+++ DSCLS/List/单链表/单链表 一次封装 整理 管炳耀.c
@@ -16,12 +16,6 @@ typedef int Status;
 #define MLC (Node *)malloc
 #define MLCS (Node *)malloc(sizeof(Node))
 */
-typedef struct Node { //封装 结构体 链表的结点==数据元素Elem,结点的指针==链表==数据对象Obj
-	ElemType data;  //数据Domain ,数据项item
-	struct Node * next; //指针,引用Reference,数据项item
-}Node, *PNode;
-//#define LNode *PNode
-//typedef PNode LNode *
 /*
 //线性表的基本操作定义声明
 
@@ -90,26 +84,27 @@ ListTraverse(L,visit()) //遍历线性表:依次对L的每个元素调用visit() 12
 /*进阶算法
 //reverseList(&L1) //逆置 单链表
 //mergeList(&L1,L2) //合并 两个线性表L 15
-//visit(e)
-//  一般是指树型链表结构中对某个节点内容进行访问的函数， 13
-//	就是取出节点内容去做某一件事，通常算法中不写出具体函数内容。
-//  树型链表结构中自顶开始按照某种顺序顺藤摸瓜至某个节点的过程称为“遍历”
-
+//visit(e) //  一般是指树型链表结构中对某个节点内容进行访问的函数， 13
+//compare(e1,e2) //比较两个元素的大小,返回Bool 14
+//compareList(L1,L2) //比较两个线性表L的大小,返回Bool 14
+//mergeList(&L1,L2) //合并两个线性表L 15
 */
-typedef struct LNode {
-	ElemType data;
-	struct LNode *next;
-}LinkList;
-Status InitList(struct LNode *L) { /* 功能：构建一个空的带头节点的单链表*/
-	(*L) = (struct LNode *)malloc(sizeof(struct LNode)); //产生头节点
+//---------------线性单链表----------------
+//单链表 初始化 创建 头插法 尾插法 插入 删除 查找 合并 长度
+typedef struct Node { //封装 结构体 链表的结点==数据元素Elem,结点的指针==链表==数据对象Obj
+	ElemType data;  //数据Domain ,数据项item
+	struct Node * next; //指针,引用Reference,数据项item
+}Node, *PNode;
+Status InitList(struct Node *L) { /* 功能：构建一个空的带头节点的单链表*/
+	(*L) = (struct Node *)malloc(sizeof(struct Node)); //产生头节点
 	if (!*L)
 		exit(OVERFLOW);
 	(*L)->next = NULL;
 	return OK;
 }
-Status DestroyList(struct LNode *L) { /*销毁线性表*/
+Status DestroyList(struct Node *L) { /*销毁线性表*/
 									  //pHead == L
-	struct LNode *q;//temp ptr, q <==> L->next
+	struct Node *q;//temp ptr, q <==> L->next
 	while (L) {
 		q = L->next;//q++
 		free(L);//free l //从头开始free(),一直到最后一个
@@ -117,8 +112,8 @@ Status DestroyList(struct LNode *L) { /*销毁线性表*/
 	}
 	return OK;
 }
-Status ClearList(struct LNode *L) { /*将L重置为空表*/
-	LinkList *p, *q;
+Status ClearList(struct Node *L) { /*将L重置为空表*/
+	Node *p, *q;
 	p = L->next;
 	while (p) {
 		q = p->next;
@@ -128,7 +123,7 @@ Status ClearList(struct LNode *L) { /*将L重置为空表*/
 	L->next = NULL;
 	return OK;
 }
-Status ListEmpty(LinkList *L) { /*判断链表是否为空表*/
+Status ListEmpty(Node *L) { /*判断链表是否为空表*/
 	if (L->next) {
 		return FALSE;
 	}
@@ -136,19 +131,19 @@ Status ListEmpty(LinkList *L) { /*判断链表是否为空表*/
 		return TRUE;
 	}
 }
-int ListLength(struct LNode *L) { /*返回单链表中元素的个数*/
+int ListLength(struct Node *L) { /*返回单链表中元素的个数*/
 	int i = 0;
-	LinkList *p = L->next;
+	Node *p = L->next;
 	while (p) {
 		i++;
 		p = p->next;
 	}
 	return i;
 }
-Status GetElem(struct LNode *L, int i, ElemType *e) {
+Status GetElem(struct Node *L, int i, ElemType *e) {
 	/* L为带头节点的单链表的头指针，当第i个元素存在时，其值赋给e,并返回OK */
 	int j = 1;
-	LinkList *p = L->next;
+	Node *p = L->next;
 	while (p && j < i) { //until i-1
 		p = p->next; //p++
 		j++;
@@ -158,11 +153,11 @@ Status GetElem(struct LNode *L, int i, ElemType *e) {
 	*e = p->data;
 	return OK;
 }
-int LocateElem(struct LNode *L, ElemType e, Status(*compare) (ElemType, ElemType)) {
+int LocateElem(struct Node *L, ElemType e, Status(*compare) (ElemType, ElemType)) {
 	/*返回L中第一个与e满足关系compare()的数据元素的位序，
 	若给存在返回值为0，compare()是数据元素的判定函数*/
 	int i = 0;
-	LinkList *p = L->next;
+	Node *p = L->next;
 	while (p) {
 		i++;
 		if (compare(p->data, e))//compare(*p,e) which SqList
@@ -171,9 +166,9 @@ int LocateElem(struct LNode *L, ElemType e, Status(*compare) (ElemType, ElemType
 	}
 	return 0;
 }
-Status PriorElem(struct LNode *L, ElemType cur_e, ElemType *pre_e) {
+Status PriorElem(struct Node *L, ElemType cur_e, ElemType *pre_e) {
 	/*所cur_e是L中的数据元素，且给就第一个，则用pre_e返回它的前驱*/
-	LinkList *q, *p = L->next;//p<==>node1 //2 assist Ptr
+	Node *q, *p = L->next;//p<==>node1 //2 assist Ptr
 	while (p->next) {
 		q = p->next;//q指向p的后继,q++, node2,no head(node0) because it's no data
 		if (q->data == cur_e) { //后继's data is matching cur_e
@@ -184,9 +179,9 @@ Status PriorElem(struct LNode *L, ElemType cur_e, ElemType *pre_e) {
 	}
 	return INFEASIBLE;
 }
-Status NextElem(struct LNode *L, ElemType cur_e, ElemType *next_e) {
+Status NextElem(struct Node *L, ElemType cur_e, ElemType *next_e) {
 	/* 若cur_e是L中的数据元素，且不是最后一个，则用next_e返回它的后继*/
-	LinkList *p; //1 assist Ptr
+	Node *p; //1 assist Ptr
 	p = L->next; //node1
 	while (p->next) { //have node
 		if (p->data == cur_e) { //cur's data is matching
@@ -197,10 +192,10 @@ Status NextElem(struct LNode *L, ElemType cur_e, ElemType *next_e) {
 	}
 	return INFEASIBLE;
 }
-Status ListInsert(struct LNode *L, int i, ElemType e) {
+Status ListInsert(struct Node *L, int i, ElemType e) {
 	/* 在带头节点的单链表L中的第i个位置之前(before i means index == i-2)插入元素e*/
 	int j = 0;
-	struct LNode *p = L, *s = NULL; //2 assist Ptr,2个辅助指针
+	struct Node *p = L, *s = NULL; //2 assist Ptr,2个辅助指针
 	while (p && j < i - 1) {
 		//Loc(i)'s index==(i-1),before i means index ==(i-2) 找到 第(i-1)个位置,插入其后
 		p = p->next;
@@ -208,7 +203,7 @@ Status ListInsert(struct LNode *L, int i, ElemType e) {
 	}
 	if (!p || j > i - 1)
 		return ERROR;
-	s = (struct LNode *)malloc(sizeof(struct LNode));
+	s = (struct Node *)malloc(sizeof(struct Node));
 	if (!s)
 		printf("malloc error~\n");
 	s->data = e;
@@ -216,9 +211,9 @@ Status ListInsert(struct LNode *L, int i, ElemType e) {
 	p->next = s;
 	return OK;
 }
-Status ListDelete(LinkList *L, int i, ElemType *e) {
+Status ListDelete(Node *L, int i, ElemType *e) {
 	/*在带头节点的单链表中删除第i个元素，并有e返回其值*/
-	LinkList *p = L, *q; //2 assist Ptr,2个辅助指针
+	Node *p = L, *q; //2 assist Ptr,2个辅助指针
 	int j = 0;
 	while (p->next && j < i - 1) { // find Loc(i-1)== find index==(i-2)
 		p = p->next; //p++
@@ -232,9 +227,9 @@ Status ListDelete(LinkList *L, int i, ElemType *e) {
 	free(q);
 	return OK;
 }
-Status ListTraverse(struct LNode *L, void(*visit)(ElemType)) {
+Status ListTraverse(struct Node *L, void(*visit)(ElemType)) {
 	/* 依次对L的每个元素调用vi(),打印输出语句*/
-	LinkList *p = L->next;
+	Node *p = L->next;
 	while (p) {
 		visit(p->data);
 		p = p->next;
@@ -242,10 +237,10 @@ Status ListTraverse(struct LNode *L, void(*visit)(ElemType)) {
 	printf("\n");
 	return OK;
 }
-void MergeList_Link_Guan(LinkList &La, LinkList &Lb) {
+void MergeList_Link_Guan(Node &La, Node &Lb) {
 	//已知La和Lb升序排列
 	//合并得到新的单链表Lc,Lc的元素也按值非递减排列
-	LinkList pa, pb, p, q;
+	Node pa, pb, p, q;
 	pa = La->next;
 	pb = Lb->next;
 	q = La;//存放临时指针,q就是pa的前驱元素,q必须始终作为pa的前驱元素
